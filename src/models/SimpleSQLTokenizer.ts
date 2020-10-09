61a04dbe354e94adb2bf45a7df35f2721220be9f
Matt Triff <matt.triff@gmail.com>
2020-10-08 20:20:59 -0400

    Update variable naming to reflect internal properties

--- src/models/SimpleSQLTokenizer.ts
+++ src/models/SimpleSQLTokenizer.ts
@@ -8,21 +8,21 @@ import { TokenSource, Token, CharStream, TokenFactory, CommonToken } from "antlr
  * 
  * It is designed to be used to find the correct token index at
  * any string location, regardless of the validity of the SQL string.
- * See SQLSurveyer.getTokenIndexAt for usage.
+ * See SQLAutocomplete.getTokenIndexAt for usage.
  */
 export class SimpleSQLTokenizer implements TokenSource {
 
   value: string;
-  currentIndex: number;
-  insideQuote: boolean;
+  _currentIndex: number;
+  _insideQuote: boolean;
 
   specialCharacters: string[] = [';', '.', '(', ')'];
   whitespaceCharacters: string[] = [' ', '\f', '\n', '\r', '\t', '\v', '\u00A0', '\u2028', '\u2029'];
 
   constructor(value: string, tokenizeWhitespace: boolean) {
     this.value = value;
-    this.currentIndex = 0;
-    this.insideQuote = false;
+    this._currentIndex = 0;
+    this._insideQuote = false;
     if (tokenizeWhitespace) {
       this.specialCharacters.push(...this.whitespaceCharacters);
     }
@@ -32,35 +32,35 @@ export class SimpleSQLTokenizer implements TokenSource {
     let start = null;
     let stop = null;
     const notWhitespaceRegex = /[^\s]/;
-    while (this.currentIndex < this.value.length) {
-      const currentChar = this.value[this.currentIndex];
-      if (currentChar === "'" && this.value[this.currentIndex - 1] !== '\\') {
-        this.insideQuote = !this.insideQuote;
+    while (this._currentIndex < this.value.length) {
+      const currentChar = this.value[this._currentIndex];
+      if (currentChar === "'" && this.value[this._currentIndex - 1] !== '\\') {
+        this._insideQuote = !this._insideQuote;
       }
-      if ((notWhitespaceRegex.test(currentChar) && !this.specialCharacters.includes(currentChar)) || this.insideQuote) {
+      if ((notWhitespaceRegex.test(currentChar) && !this.specialCharacters.includes(currentChar)) || this._insideQuote) {
         if (start === null) {
-          start = this.currentIndex;
+          start = this._currentIndex;
         }
-        if (this.currentIndex === this.value.length - 1) {
-          stop = this.currentIndex;
+        if (this._currentIndex === this.value.length - 1) {
+          stop = this._currentIndex;
         }
       } else if (start !== null) {
-        stop = this.currentIndex - 1;
+        stop = this._currentIndex - 1;
         if (this.specialCharacters.includes(currentChar)) {
           // The next block will iterate past the current special character
           // Need to back up so that on the next call to nextToken, the special character will be identified again
-          this.currentIndex--;
+          this._currentIndex--;
         }
       }
       if (start !== null && stop !== null) {
-        this.currentIndex++;
+        this._currentIndex++;
         return new CommonToken(Token.DEFAULT_CHANNEL, this.value.substring(start, stop + 1), {}, null, start, stop);
       }
-      if (this.specialCharacters.includes(currentChar) && !this.insideQuote) {
-        this.currentIndex++;
-        return new CommonToken(Token.DEFAULT_CHANNEL, currentChar, {}, null, this.currentIndex - 1, this.currentIndex - 1);
+      if (this.specialCharacters.includes(currentChar) && !this._insideQuote) {
+        this._currentIndex++;
+        return new CommonToken(Token.DEFAULT_CHANNEL, currentChar, {}, null, this._currentIndex - 1, this._currentIndex - 1);
       }
-      this.currentIndex++;
+      this._currentIndex++;
     }
     return new CommonToken(Token.EOF);
   }
